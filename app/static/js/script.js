427ab602ef1d50e7e72cb5d3e53b76eebc2646a1
Abidur Rahman <144458543+urabidurr@users.noreply.github.com>
2025-01-16 22:55:13 -0500

    Update script.js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,22 +3,12 @@ const swiper = document.querySelector('#swiper');
 const like = document.querySelector('#like');
 const dislike = document.querySelector('#dislike');
 
-// constants
-const urls = [
-  'https://source.unsplash.com/random/1000x1000/?sky',
-  'https://source.unsplash.com/random/1000x1000/?landscape',
-  'https://source.unsplash.com/random/1000x1000/?ocean',
-  'https://source.unsplash.com/random/1000x1000/?moutain',
-  'https://source.unsplash.com/random/1000x1000/?forest'
-];
-
 // variables
 let cardCount = 0;
 
 // functions
 function appendNewCard() {
   const card = new Card({
-    imageUrl: urls[cardCount % 5],
     onDismiss: appendNewCard,
     onLike: () => {
       like.style.animationPlayState = 'running';
